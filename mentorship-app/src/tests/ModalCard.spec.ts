24f7e8db4ada2d3fd08709f0202020ddf1d4f787
Oleksandr Sheremet <o.sheremet@viseven.com>
2026-04-07 18:56:19 +0300

    update(tests): add test for Modal (emit likes, dislikes)

---
+++ mentorship-app/src/tests/ModalCard.spec.ts
@@ -0,0 +1,30 @@
+import { shallowMount } from '@vue/test-utils';
+import ModalCard from '@/components/ModalCard.vue';
+
+describe('ModalCard.vue', () => {
+  const props = {
+    visible: true,
+    image: 'test.png',
+    likes: 5,
+    dislikes: 2,
+    initialComments: [],
+  };
+
+  it('emits "like" event when like button clicked', async () => {
+    const wrapper = shallowMount(ModalCard, { propsData: props });
+
+    const likeButton = wrapper.find('.like-box');
+    await likeButton.trigger('click');
+
+    expect(wrapper.emitted('like')).toBeTruthy();
+  });
+
+  it('emits "dislike" event when dislike button clicked', async () => {
+    const wrapper = shallowMount(ModalCard, { propsData: props });
+
+    const dislikeButton = wrapper.find('.dislike-box');
+    await dislikeButton.trigger('click');
+
+    expect(wrapper.emitted('dislike')).toBeTruthy();
+  });
+});
